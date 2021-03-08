6cbbd454b217477f0873a65ff9ddf9aad38cc0ee
KartikJ06 <74360906+KartikJ06@users.noreply.github.com>
2021-03-08 18:16:46 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,38 @@
+class Box{
+    constructor(x,y,width,height) {
+        var options = {
+            'restitution': 0.8,
+            'friction': 1.0,
+            'density': 1.0 
+        }
+    
+    this.body = Bodies.rectangle(x,y,width,height,options);
+    this.width = width; 
+    this.height = height; 
+    World.add(world, this.body);  
+   // this.Visibility = 255; 
+    }
+
+    display(){
+        //if(this.body.speed < 3){
+          //  super.display(); 
+        //}
+
+        //else {
+          //  World.remove(world,this.body);
+            //push();
+            //this.Visibility = this.Visibility - 5; 
+           // TransitionEvent(255,this.Visibility);
+           // pop(); 
+        //}
+        
+        var angle = this.body.angle; 
+        fill("black"); 
+        push(); 
+        translate(this.body.position.x,this.body.position.y); 
+        rotate(angle); 
+        rectMode(CENTER); 
+        rect(0,0,this.width,this.height); 
+        pop(); 
+    }
+}
